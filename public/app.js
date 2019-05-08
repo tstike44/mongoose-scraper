9cfc247fd27744227d0f99446df7838f9b5898b5
tstike44 <you@example.com>
2019-05-07 22:03:08 -0700

    ask for help..ur dying

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    $("#articles").append("<button class='submit'>Submit</button>");
+    $("#articles").append("<button class='submit'>Save</button>");
   }
 });
 
